be80dff49eb7f0b7623921b1bdeded6155a0beda
Deryan Argueta <deryan.hndz@gmail.com>
2018-10-03 14:30:18 -0600

    Improved SV in progress

--- app/src/main/java/com/example/applaudo/newsapp/fragments/NewsFragment.java
+++ app/src/main/java/com/example/applaudo/newsapp/fragments/NewsFragment.java
@@ -2,12 +2,12 @@ package com.example.applaudo.newsapp.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -17,7 +17,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.applaudo.newsapp.R;
@@ -31,6 +30,8 @@ import com.example.applaudo.newsapp.query.Query;
 import java.util.ArrayList;
 
 public class NewsFragment extends Fragment implements NewsAdapter.OnNewsClicked, LoaderManager.LoaderCallbacks<ArrayList<News>> {
+
+    public static String LOADER_SEARCH_ARGS = "LOADER_SEARCH_ARGS";
 
     NewsAdapter mAdapter;
 
@@ -46,8 +47,24 @@ public class NewsFragment extends Fragment implements NewsAdapter.OnNewsClicked,
                 return false;
             }
             @Override
-            public boolean onQueryTextChange(String newText) {
-                mAdapter.getFilter().filter(newText);
+            public boolean onQueryTextChange(final String newText) {
+
+                //This is to initialize the search one second after the last key is pressed
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Creates a new loader with another ID to handle the search
+                        LoaderManager loaderManager = getLoaderManager();
+
+                        //Stores the text and sends it to the onCreateLoader method
+                        Bundle loaderData = new Bundle();
+                        loaderData.putString(LOADER_SEARCH_ARGS,newText);
+
+                        loaderManager.initLoader(2,loaderData,NewsFragment.this);
+                    }
+                },1000);
+
                 return false;
             }
         });
@@ -91,24 +108,31 @@ public class NewsFragment extends Fragment implements NewsAdapter.OnNewsClicked,
         //Create a new loader from the given URL's
 
         //Retrieve the tab position from the PagerAdapter
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            int tabPosition = bundle.getInt(MainActivity.TAB);
-            //Return different loaders with different URL's based on tab position
-            switch (tabPosition) {
-                case 0:
-                    return new NewsLoader(getContext(), Query.QUERY_SPORTS);
-                case 1:
-                    return new NewsLoader(getContext(), Query.QUERY_POLITICS);
-                case 2:
-                    return new NewsLoader(getContext(), Query.QUERY_SOCIAL);
-                default:
-                    return new NewsLoader(getContext(), Query.QUERY_MUSIC);
+        if(id==1) {
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                int tabPosition = bundle.getInt(MainActivity.TAB);
+                //Return different loaders with different URL's based on tab position
+                switch (tabPosition) {
+                    case 0:
+                        return new NewsLoader(getContext(), Query.QUERY_SPORTS);
+                    case 1:
+                        return new NewsLoader(getContext(), Query.QUERY_POLITICS);
+                    case 2:
+                        return new NewsLoader(getContext(), Query.QUERY_SOCIAL);
+                    default:
+                        return new NewsLoader(getContext(), Query.QUERY_MUSIC);
+                }
+            } else {
+                //It shouldn't ever get in here, but it was required because of the "if" statement
+                return null;
             }
         } else {
-            //It shouldn't ever get in here, but it was required because of the "if" statement
-            return null;
+            //Id should be "2" at this point
+            Toast.makeText(getContext(), args.getString(LOADER_SEARCH_ARGS), Toast.LENGTH_SHORT).show();
+            return new NewsLoader(getContext(), Query.QUERY_SEARCH);
         }
+
 
     }
 
@@ -120,6 +144,7 @@ public class NewsFragment extends Fragment implements NewsAdapter.OnNewsClicked,
 
     @Override
     public void onLoaderReset(Loader<ArrayList<News>> loader) {
+        Toast.makeText(getContext(), "Reset", Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/example/applaudo/newsapp/query/Query.java
+++ app/src/main/java/com/example/applaudo/newsapp/query/Query.java
@@ -5,4 +5,6 @@ public class Query {
     public static final String QUERY_SPORTS = "https://content.guardianapis.com/search?show-fields=headline%2CbodyText%2Cthumbnail%2CshortUrl&page-size=30&q=sport&api-key=3f05b3d1-6a19-44ff-a6fa-6c0e825e314b";
     public static final String QUERY_MUSIC = "https://content.guardianapis.com/search?show-fields=headline%2CbodyText%2Cthumbnail%2CshortUrl&page-size=30&q=music&api-key=3f05b3d1-6a19-44ff-a6fa-6c0e825e314b";
     public static final String QUERY_SOCIAL = "https://content.guardianapis.com/search?show-fields=headline%2CbodyText%2Cthumbnail%2CshortUrl&page-size=30&q=social&api-key=3f05b3d1-6a19-44ff-a6fa-6c0e825e314b";
+    public static final String QUERY_SEARCH = "https://content.guardianapis.com/search?show-fields=headline%2CbodyText%2Cthumbnail%2CshortUrl&page-size=30&q=all&api-key=3f05b3d1-6a19-44ff-a6fa-6c0e825e314b";
+
 }
